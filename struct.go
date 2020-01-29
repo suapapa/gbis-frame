39b52d016b6d793b49ab0583d7b8396fc2aa7336
Homin Lee <homin.lee@suapapa.net>
2020-01-30 00:05:17 +0900

    add struct of bus arrival response

--- struct.go
+++ struct.go
@@ -29,3 +29,36 @@ type BaseInfoResponse struct {
 		} `xml:"baseInfoItem"`
 	} `xml:"msgBody"`
 }
+
+// BusArrivalResponse represents response of http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station
+type BusArrivalResponse struct {
+	XMLName      xml.Name `xml:"response"`
+	ComMsgHeader struct {
+		ErrMsg     string `xml:"errMsg"`
+		ReturnCode string `xml:"returnCode"`
+	} `xml:"comMsgHeader"`
+	MsgHeader struct {
+		QueryTime     string `xml:"queryTime"`
+		ResultCode    string `xml:"resultCode"`
+		ResultMessage string `xml:"resultMessage"`
+	} `xml:"msgHeader"`
+	MsgBody struct {
+		BusArrivalList []struct {
+			Text           string `xml:",chardata"`
+			Flag           string `xml:"flag"`
+			LocationNo1    string `xml:"locationNo1"`
+			LocationNo2    string `xml:"locationNo2"`
+			LowPlate1      string `xml:"lowPlate1"`
+			LowPlate2      string `xml:"lowPlate2"`
+			PlateNo1       string `xml:"plateNo1"`
+			PlateNo2       string `xml:"plateNo2"`
+			PredictTime1   string `xml:"predictTime1"`
+			PredictTime2   string `xml:"predictTime2"`
+			RemainSeatCnt1 string `xml:"remainSeatCnt1"`
+			RemainSeatCnt2 string `xml:"remainSeatCnt2"`
+			RouteID        string `xml:"routeId"`
+			StaOrder       string `xml:"staOrder"`
+			StationID      string `xml:"stationId"`
+		} `xml:"busArrivalList"`
+	} `xml:"msgBody"`
+}
