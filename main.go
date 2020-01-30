4aa42a763ba1814f5d0a7f49d988232e8b461834
Homin Lee <homin.lee@suapapa.net>
2020-01-30 14:57:09 +0900

    test bus arrival service station

--- main.go
+++ main.go
@@ -20,14 +20,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
-
+	// defer resp.Body.Close()
 	var r BaseInfoResponse
 	xmlDec := xml.NewDecoder(resp.Body)
 	xmlDec.Decode(&r)
-
 	fmt.Println(r)
+	// downloadBaseInfos(&r)
+	resp.Body.Close()
 
-	downloadBaseInfos(&r)
+	// TODO: get station ID from mobile key (5 digits) from stationYYYYMMDD.txt
+	stationID := "206000678" // H스퀘어
+	resp, err = http.Get(urlBusArrivalServiceStation + fmt.Sprintf("?serviceKey=%s&stationId=%s", serviceKey, stationID))
+	if err != nil {
+		panic(err)
+	}
+	// defer resp.Body.Close()
+	var sr BusArrivalStationResponse
+	xmlDec = xml.NewDecoder(resp.Body)
+	xmlDec.Decode(&sr)
+	for _, item := range sr.MsgBody.BusArrivalList {
+		fmt.Println("routeId", item.RouteID) // TODO: routeId to bus No. from routeYYYYMMDD.txt
+		fmt.Println("PredictTime1", item.PredictTime1)
+		fmt.Println("PredictTime2", item.PredictTime2)
+		fmt.Println("----")
+	}
+
+	resp.Body.Close()
 
 }
