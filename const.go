1c1888f88f8483ef06c12624e57d5bba958427da
Homin Lee <homin.lee@suapapa.net>
2020-02-02 23:21:39 +0900

    deperecating base info...

--- const.go
+++ const.go
@@ -1,7 +1,8 @@
 package main
 
 const (
-	urlBaseInfoService          = "http://openapi.gbis.go.kr/ws/rest/baseinfoservice"
-	urlBusArrivalService        = "http://openapi.gbis.go.kr/ws/rest/busarrivalservice"         // 버스 도착정보 항목조회
+	urlBaseInfoService          = "http://openapi.gbis.go.kr/ws/rest/baseinfoservice"           // TODO: will be deperecated
+	urlBusStationService        = "http://openapi.gbis.go.kr/ws/rest/busstationservice"         // 정류소 조회 서비스 -> 정류소명/번호 목록조회
+	urlBusRouteServiceInfo      = "http://openapi.gbis.go.kr/ws/rest/busrouteservice/info"      // 버스노선 조회 서비스 -> 노선정보 항목조회
 	urlBusArrivalServiceStation = "http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station" // 버스 도착정보 목록조회
 )
