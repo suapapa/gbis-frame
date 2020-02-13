361de366f60fc1df4cf5c5d02ade799ae305b04f
Homin Lee <homin.lee@suapapa.net>
2020-02-13 18:57:47 +0900

    add python helper
    code from https://github.com/waveshare/e-Paper

---
+++ _python/epd7in5_update.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5 Demo")
+    
+    epd = epd7in5.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    # epd.Clear()
+    
+    logging.info("3.read bmp file")
+    Himage = Image.open(sys.argv[1])
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5.epdconfig.module_exit()
+    exit()
